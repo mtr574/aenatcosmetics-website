a6ac11987928709723fccbee0e5011b27e3380f8
Nate.Ben <mtr574@gmail.com>
2017-04-10 12:57:45 +0300

    grunt setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,54 +2,98 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    clean: [
-      "dist", '.tmp'
-    ],
-
     copy: {
       main: {
         expand: true,
-        cwd: 'app/',
+        cwd: 'public/',
         src: [
-          '**', '!js/**', '!lib/**', '!**/*.css'
+          '**', '!js/**', '!css/**', '!bower_components/**'
         ],
         dest: 'dist/'
       }
     },
-
-    rev: {
-      files: {
-        src: ['dist/**/*.{js,css}', '!dist/js/shims/**']
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
+      app: {
+        files: {
+          'public/app/app.js': ['dist/app/app.js']
+        }
       }
     },
-
     useminPrepare: {
-      html: 'app/index.html'
+      html: 'public/index.html'
     },
-
     usemin: {
       html: ['dist/index.html']
     },
-
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true
+      },
+      vendors: {
+        src: [
+          'bower_components/jquery/dist/jquery.min.js', 'bower_components/angular/angular.min.js', 'js/modernizr.min.js'
+        ],
+        dest: 'dist/js/vendors.js'
+      },
+      init: {
+        src: [
+          '!js/modernizr.min.js', 'js/*.js'
+        ],
+        dest: 'dist/js/init.js'
+      },
+      app: {
+        src: [
+          'app/app.js', 'app/controllers/*.js'
+        ],
+        dest: 'dist/app/app.js'
+      }
+    },
     uglify: {
       options: {
         report: 'min',
         mangle: false
       }
-    }
+    },
+    cssmin: {
+      target: {
+        files: [
+          {
+            expand: true,
+            cwd: 'css',
+            src: [
+              '*.css', '!*.min.css'
+            ],
+            dest: 'dist/css',
+            ext: '.min.css'
+          }
+        ]
+      }
+    },
+    es6transpiler: {
+      dist: {
+        files: {
+          'dist/app/app.js': 'dist/app/app.js'
+        }
+      }
+    },
+    clean: ['.tmp']
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Tell Grunt what to do when we type "grunt" into the terminal
   grunt.registerTask('default', [
     'copy',
+    'ngAnnotate',
+    'es6transpiler',
     'useminPrepare',
-    'concat',
-    'uglify',
-    'cssmin',
-    'rev',
-    'usemin'
+    'concat:generated',
+    'uglify:generated',
+    'cssmin:generated',
+    'usemin',
+    'clean'
   ]);
 };
